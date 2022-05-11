13e72cf4203ce0f2cd78a9f731e3f6a6c584fbc0
Misha Hrysiuk <0973853103h@gmail.com>
2022-05-11 19:58:37 +0300

    modificate function in Object and addtoggleVisibleMyDB to object

--- js/script.js
+++ js/script.js
@@ -1,68 +1,67 @@
-let numberOfFilms;
-
-start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt("How much has you watched films?", '');
+
+        while (!personalMovieDB.count || personalMovieDB.count < 0) {
+            personalMovieDB.count = +prompt("How much has you watched films?", '');
+        }
+    },
+    rememberMyFilms: function(numberOfQuestions) {
+        let i = 0;
+        while (i < numberOfQuestions){
+            const lastFilm = prompt("One of the last films?", ''),
+            rateFilm = +prompt("Rate the film", '');
+            if (lastFilm == null || !lastFilm.trim() || !rateFilm || rateFilm < 0 || rateFilm > 10 || lastFilm.length >= 50) {
+                continue;
+            }
+            personalMovieDB.movies[lastFilm] = rateFilm;
+            i++;
+        }
+    },
+    detectPersonalLevel: function(){
+        if (personalMovieDB.count < 10) {
+            alert('A fewness watched filmes');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count <=30) {
+            alert('You are classic viewer');
+        } else if (personalMovieDB.count > 30) {
+            alert('You are MOVIEGOER!!!');
+        } else {
+            alert('Something wrong!');
+        }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function () {
+        personalMovieDB.privat = !personalMovieDB.privat;
+    },
+    writeYourGenres: function(numberOfGenres) {
+        for (let i = 0; i < numberOfGenres; i++){
+            personalMovieDB.genres[i] = prompt(`Your favorite ganre of number ${i + 1}`, '');
+            if (!personalMovieDB.genres[i] || personalMovieDB.genres[i].trim() == '') {
+                i--;
+            }
+        }
+        personalMovieDB.genres.forEach((genre, i) => console.log(`Favourite ganre #${i+1} is '${genre}'\n`));
+    }
 };
 
-const numberOfQuestions = 2;
-const numberOfGenres = 3;
+// const numberOfQuestions = 2;
+// const numberOfGenres = 3;
 
-rememberMyFilms(numberOfQuestions);
+// personalMovieDB.rememberMyFilms(numberOfQuestions);
 
-detectPersonalLevel();
+// personalMovieDB.detectPersonalLevel();
 
-writeYourGenres(numberOfGenres);
+// personalMovieDB.writeYourGenres(numberOfGenres);
 
-showMyDB(personalMovieDB.privat);
+// personalMovieDB.toggleVisibleMyDB();
 
-// FUNCTION
-function start() {
-    numberOfFilms = +prompt("How much did you watched films?", '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-            numberOfFilms = +prompt("How much did you watched films?", '');
-    }
-}
-
-function rememberMyFilms(numberOfQuestions) {
-    let i = 0;
-    while (i < numberOfQuestions){
-        const lastFilm = prompt("One of the last films?", ''),
-         rateFilm = +prompt("Rate a film", '');
-        if (!lastFilm || lastFilm.length >= 50) {
-            continue;
-        }
-        personalMovieDB.movies[lastFilm] = rateFilm;
-        i++;
-    }
-}
-
-function detectPersonalLevel(){
-    if (personalMovieDB.count < 10) {
-        alert('A fewness watched filmes');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <=30) {
-        alert('You are classic viewer');
-    } else if (personalMovieDB.count > 30) {
-        alert('You are MOVIEGOER!!!');
-    } else {
-        alert('Something wrong!');
-    }
-}
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-function writeYourGenres(numberOfGenres) {
-    for (let i = 0; i < numberOfGenres; i++){
-        personalMovieDB.genres[i] = prompt(`Your favorite ganre of number ${i + 1}`, '');
-    }
-}
+// personalMovieDB.showMyDB(personalMovieDB.privat);
